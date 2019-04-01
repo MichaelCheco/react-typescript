f28d4a4a90a8b1a01019456c504085312c5e4ce7
Michael <checomichael2@gmail.com>
2019-04-01 13:41:10 -0400

    Tuples

--- FEM.ts
+++ FEM.ts
@@ -1,1 +1,10 @@
 // Frontend Masters TypeScript Fundamentals
+
+// A tuple is an array of a fixed length that comes with a convention
+
+let bb: [number, string, string, number] = [
+	123,
+	'Fake Street',
+	'Nowhere, USA',
+	10110,
+];
