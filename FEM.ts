20d5ee2a01ec004c5fd40b1fbb0540705a6ec0f1
Michael <checomichael2@gmail.com>
2019-04-01 13:51:21 -0400

    Intersection and Union operators

--- FEM.ts
+++ FEM.ts
@@ -8,3 +8,43 @@ let bb: [number, string, string, number] = [
 	'Nowhere, USA',
 	10110,
 ];
+
+// Think of an Interface as a name for a structure that we are creating, works well object types
+
+export interface HasPhoneNumber {
+	name: string;
+	phone: number;
+}
+export interface HasEmail {
+	name: string;
+	email: string;
+}
+
+// Intersection And Union Operators & - |
+let contactInfo: HasEmail | HasPhoneNumber =
+	Math.random() > 0.5
+		? {
+				// we can assign it to a HasPhoneNumber
+				name: 'Mike',
+				phone: 312413423,
+		  }
+		: {
+				// or a HasEmail
+				name: 'Mike',
+				email: 'email@email.com',
+		  };
+
+contactInfo.name; // NOTE: We can only access the .name property
+
+// Union Types
+
+let otherContactInfo: HasEmail & HasPhoneNumber = {
+	// we must initialize it with all needed values
+	name: 'Mike',
+	email: 'email@email.com',
+	phone: 21321433,
+};
+
+otherContactInfo.name; // NOTE: We can access anything on _either
+otherContactInfo.email;
+otherContactInfo.phone;
