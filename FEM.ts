f12fc76f4856aaa9cc6802d0d523f870094f878d
Michael <checomichael2@gmail.com>
2019-04-01 15:06:22 -0400

    Functions

--- FEM.ts
+++ FEM.ts
@@ -1,3 +1,5 @@
+import { invoke } from 'q';
+
 // Frontend Masters TypeScript Fundamentals
 
 // A tuple is an array of a fixed length that comes with a convention
@@ -61,3 +63,100 @@ otherContactInfo.phone;
  * ... ["abc", 'def, string]
  * NOTHING never
  */
+
+// FUNCTIONS
+
+// (1) Function arguments and return values can have type annotations
+
+function sendEmail(to: HasEmail): { recipient: string; body: string } {
+	return {
+		recipient: `${to.name} <${to.email}>`,
+		body: "You're pre-qualified for a loan",
+	};
+}
+
+// (2) arrow-function variant
+const sendTextMessage = (
+	to: HasPhoneNumber
+): { recipient: string; body: string } => {
+	return {
+		recipient: `${to.name} <${to.phone}>`,
+		body: "You're pre-qualified for a loan",
+	};
+};
+
+// (3) Return types can almost always be inferred
+
+function getNameParts(contact: { name: string }) {
+	const parts = contact.name.split(/\s/g); // split @ whitespace
+	if (parts.length < 2) {
+		throw new Error(`Can't calculate name parts from name ${contact.name}`);
+	}
+	return {
+		first: parts[0],
+		middle:
+			parts.length === 2
+				? undefined
+				: //everything except first and last
+				  parts.slice(1, parts.length - 2).join(' '),
+		last: parts[parts.length - 1],
+	};
+}
+
+// (4) rest params work just as you'd think, Type must be array-ish
+const sum = (...vals: number[]) => vals.reduce((sum, x) => sum + x, 0);
+
+// (5) We can even provide multiple function signatures
+// 'overload signatures'
+
+function contactPeople(method: 'email', ...people: HasEmail[]): void;
+function contactPeople(method: 'phone', ...people: HasPhoneNumber[]): void;
+
+// IMPLEMENTATION
+function contactPeople(
+	method: 'email' | 'phone',
+	...people: (HasEmail | HasPhoneNumber)[]
+): void {
+	if (method === 'email') {
+		(people as HasEmail[]).forEach(sendEmail);
+	} else {
+		(people as HasPhoneNumber[]).forEach(sendTextMessage);
+	}
+}
+
+// ✅ email works
+contactPeople('email', { name: 'foo', email: 'd' });
+// ✅ phone works
+contactPeople('phone', { name: 'foo', phone: 2222222 });
+// ❌ mixing does not work
+contactPeople('email', { name: 'foo', phone: 2222222 });
+
+// (6) The lexical scope (this of a function is part of its signature
+
+function sendMessage(
+	this: HasEmail & HasPhoneNumber,
+	prefferedMethod: 'phone' | 'email'
+) {
+	if (prefferedMethod === 'email') {
+		console.log('send email');
+		sendEmail(this);
+	} else {
+		console.log('text message');
+		sendTextMessage(this);
+	}
+}
+
+const c = { name: 'Mike', phone: 123421412, email: 'email@email.com' };
+
+function invokeSoon(cb: () => any, timeout: number) {
+	setTimeout(() => cb.call(null, timeout));
+}
+// ❌ this is not satisfied
+invokeSoon(() => sendMessage('email', 500));
+
+// ✅ creating a bound function is one solution
+const bound = sendMessage.bind(c, ' email');
+invokeSoon(() => bound(), 500);
+
+// ✅ call/apply work as well
+invokeSoon(() => sendMessage.apply(c, ['phone']), 500);
